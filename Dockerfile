33f59c321627157356f9d08aa4164ab10a966190
Jakob <jakobniggel@gmail.com>
2023-02-07 14:08:19 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,7 @@ FROM amazonlinux:2022
 
 RUN yum install -y systemd sudo procps
 RUN yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+
+ADD entrypoint.sh /
+
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [[ -z $SSM_CODE || -z $SSM_ID || -z $SSM_REGION ]]; then
+  echo "SSM_CODE, SSM_ID and SSM_REGION must be provided as environment variable"
+  exit 1
+fi
+
 if [[ ! -f "/var/lib/amazon/ssm/registration" ]]; then
-  echo "yes" | amazon-ssm-agent -register -code "$SSM_CODE" -id "$SSM_ID" -region "$SSM_REGION" 
+  echo "Registering machine..."
+  echo "yes" | amazon-ssm-agent -register -code "$SSM_CODE" -id "$SSM_ID" -region "$SSM_REGION"
 fi
 
 amazon-ssm-agent
