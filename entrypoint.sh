a1092256ff73e030941e122c48e5b7a2c28f6ab9
Jakob <jakobniggel@gmail.com>
2023-02-09 15:45:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,28 @@ if [[ -z $SSM_CODE || -z $SSM_ID || -z $SSM_REGION ]]; then
   exit 1
 fi
 
+mkdir /host/opt/amazon/bin/ || echo "/opt/amazon/bin/ already exists..."
+
+cp /usr/bin/amazon-ssm-agent /host/opt/amazon/bin/
+cp /usr/bin/ssm-* /host/opt/amazon/bin/
+cp amazon-ssm-agent.service /host/etc/systemd/system/
+
+cat > /host/opt/amazon/bin/amazon-ssm-init.sh <<EOF
+#!/bin/bash
+
+export SSM_ID=$SSM_ID
+export SSM_REGION=$SSM_REGION
+export SSM_CODE=$SSM_CODE
+
 if [[ ! -f "/var/lib/amazon/ssm/registration" ]]; then
   echo "Registering machine..."
-  echo "yes" | amazon-ssm-agent -register -code "$SSM_CODE" -id "$SSM_ID" -region "$SSM_REGION"
+  echo "yes" | /opt/amazon/bin/amazon-ssm-agent -register -code "$SSM_CODE" -id "$SSM_ID" -region "$SSM_REGION"
 fi
+systemctl enable amazon-ssm-agent
+systemctl start amazon-ssm-agent
+EOF
 
-amazon-ssm-agent
+chmod +x /host/opt/amazon/bin/amazon-ssm-init.sh
+chroot /host/ /opt/amazon/bin/amazon-ssm-init.sh
+
+tail -f /var/log/amazon/ssm/amazon-ssm-agent.log
